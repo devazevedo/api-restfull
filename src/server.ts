afed4caf09f611d9db3e950700bc07beb00df577
Matheus Wilberstaedt <azevedoafc23@gmail.com>
2024-05-09 20:58:15 -0300

    changes to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { env } from './env'
 import { app } from './app'
 
+const port = env.PORT || 3333; // Definindo a porta com base em env.PORT ou 3333 como padrão
+
 app
-  .listen({
-    port: env.PORT || 3333,
-  })
+  .listen({ port })
   .then(() => {
-    console.log(`Server is running on port ${env.PORT}`)
-  })
+    console.log(`Server is running on port ${port}`);
+  });
